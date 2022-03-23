6614e540456307697ef03bdf18cf936145e688f4
wutl <tlwu@njmlab.com>
2022-03-23 15:33:08 +0800

    test

--- install_scripts/installLinux.sh
+++ install_scripts/installLinux.sh
@@ -9,7 +9,7 @@ fi
 
 pip install --upgrade pip
 pip install -i https://hosted.chia.net/simple/ miniupnpc==2.1 setproctitle==1.1.10
-pip install git+https://github.com/kiwihealthcare-network/kiwi-blockchain.git@v0.3.0
+pip install git+https://github.com/kiwihealthcare-network/kiwi-blockchain.git@v1.0.0
 
 
 echo -e "\n===================================================="
