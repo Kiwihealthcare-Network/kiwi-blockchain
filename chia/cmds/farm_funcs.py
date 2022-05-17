4a433c973ca9e2badbfa2b9b17afb8254a444b9c
kiwidev001 <kiwidev001@kiwi.com>
2022-05-17 15:46:52 +0800

    test

--- chia/cmds/farm_funcs.py
+++ chia/cmds/farm_funcs.py
@@ -334,8 +334,8 @@ async def get_est_staking_factor(total_plot_size, total_staking_balance) -> Deci
         return Decimal(1)
 
     # convert farmer space from byte to T unit
-    # converted_plot_size = total_plot_size / 1000000000000
-    converted_plot_size = total_plot_size / 1000000
+    converted_plot_size = total_plot_size / 1000000000000
+    # converted_plot_size = total_plot_size / 1000000
 
     if total_staking_balance >= converted_plot_size:
         sf = Decimal("0.5") + Decimal(1) / ((Decimal(total_staking_balance) * 200) / Decimal(converted_plot_size) + Decimal(1))
